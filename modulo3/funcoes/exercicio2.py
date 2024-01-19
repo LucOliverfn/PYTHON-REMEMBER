9da4b9d50274740f469c36234d7cab0825e93e38
LucOliverfn <lucas.ofn@gmail.com>
2024-01-19 15:16:02 -0300

    exercicios funções

---
+++ modulo3/funcoes/exercicio2.py
@@ -0,0 +1,8 @@
+def mostraLinha(msg):
+    print("-"*40)
+    print(f'          {msg}            ')
+    print("-"*40)
+
+mostraLinha("Sistema de Alunos")
+mostraLinha('Cadastro de Funcionario')
+mostraLinha('Erro no Sistema')
